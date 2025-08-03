a41be5003173787f9bed0e97a0322317768df3b9
Decency Anukam <124494565+Deecency@users.noreply.github.com>
2025-08-03 12:23:25 +0100

    Reintroduce APK file renaming in build process
    Restore the functionality to rename the APK file after the build
    process. This change ensures that the APK is correctly named using
    the client identifier and version from the pubspec file.
    
    - Added back the APK renaming logic
    - Ensured that the renaming occurs in both build types

--- lib/build.dart
+++ lib/build.dart
@@ -166,11 +166,6 @@ class BuildCommand extends UdaraCommand {
         await runShell(
           'flutter build $buildType --dart-define=CLIENT_ENV=".env"',
         );
-
-        print('\n--- 📝 Renaming APK file ---');
-        final version = await helper.getVersionFromPubspec();
-        await helper.renameApk('${client}_$version', type);
-        print('✅ APK renamed to: ${client}_$version.$type');
       } else {
         await runShell(
           'flutter build ipa --dart-define=CLIENT_ENV=".env"',
@@ -188,7 +183,12 @@ class BuildCommand extends UdaraCommand {
       print('\n--- 🧹 Final Phase: Cleaning Up ---');
 
       await helper.cleanup();
-      if (slackService != null && version != null) {
+      print('\n--- 📝 Renaming APK file ---');
+      final version = await helper.getVersionFromPubspec();
+      await helper.renameApk('${client}_$version', type);
+      print('✅ APK renamed to: ${client}_$version.$type');
+
+      if (slackService != null) {
         final buildDuration = DateTime.now().difference(buildStartTime!);
         await slackService!.sendBuildSummary(
           client: client,
